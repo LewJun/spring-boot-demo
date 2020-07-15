becefd06acfba5956f67f0fa6f65d11b88b9dd6d
LewJun <lewjun072@sina.com>
2020-07-15 22:43:35 +0800

    若为null，则不要返回给前端 @JsonInclude(JsonInclude.Include.NON_NULL)

--- spring-boot-recv-param/src/main/java/com/example/lewjun/model/BaseObj.java
+++ spring-boot-recv-param/src/main/java/com/example/lewjun/model/BaseObj.java
@@ -1,4 +1,9 @@
 package com.example.lewjun.model;
 
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+// 若为null，则不要返回给前端
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseObj {
 }
