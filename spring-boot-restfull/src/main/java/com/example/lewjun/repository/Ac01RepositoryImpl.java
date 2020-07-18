f42deb92598bdc00595b38367d0bd440e95f1363
LewJun <lewjun072@sina.com>
2020-07-18 18:47:46 +0800

    完善Ab01和model注释

---
+++ spring-boot-restfull/src/main/java/com/example/lewjun/repository/Ac01RepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.example.lewjun.repository;
+
+import com.example.lewjun.domain.Ac01;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class Ac01RepositoryImpl implements Ac01Repository {
+    private final Map<Integer, Ac01> repository = Collections.synchronizedMap(new HashMap<>());
+
+    @Override
+    public List<Ac01> getList() {
+        return new ArrayList<>(repository.values());
+    }
+
+    @Override
+    public void save(final Ac01 ac01) {
+        final int nextAac001 = repository.isEmpty() ? 1 : Collections.max(repository.keySet()) + 1;
+        ac01.setAac001(nextAac001);
+        repository.put(nextAac001, ac01);
+    }
+
+    @Override
+    public Ac01 get(final int aac001) {
+        return repository.get(aac001);
+    }
+
+    @Override
+    public void delete(final int aac001) {
+        repository.remove(aac001);
+    }
+
+    @Override
+    public void update(final Ac01 ac01) {
+        final int aac001 = ac01.getAac001();
+        if (repository.containsKey(aac001)) {
+            repository.put(aac001, ac01);
+        } else {
+            throw new IllegalArgumentException("更新失败");
+        }
+    }
+}
