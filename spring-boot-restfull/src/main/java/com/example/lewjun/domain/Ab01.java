64d091ba8abc99bd45819c39b9b3f24f01137a7f
LewJun <lewjun072@sina.com>
2020-07-18 18:47:27 +0800

    完善Ab01和model注释

--- spring-boot-restfull/src/main/java/com/example/lewjun/domain/Ab01.java
+++ spring-boot-restfull/src/main/java/com/example/lewjun/domain/Ab01.java
@@ -6,7 +6,16 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 public class Ab01 extends BaseObj {
+    /**
+     * 部门编号
+     */
     private int aab001;
+    /**
+     * 部门名称
+     */
     private String aab002;
+    /**
+     * 部门所在位置
+     */
     private String aab003;
 }
--- spring-boot-restfull/src/main/java/com/example/lewjun/domain/Ac01.java
+++ spring-boot-restfull/src/main/java/com/example/lewjun/domain/Ac01.java
@@ -11,16 +11,52 @@ import java.util.List;
 @Data
 @Accessors(chain = true)
 public class Ac01 extends BaseObj {
+    /**
+     * 雇员编号
+     */
     private int aac001;
+    /**
+     * 雇员姓名
+     */
     private String aac002;
+    /**
+     * 雇员职位
+     */
     private String aac003;
+    /**
+     * 领导编号
+     */
     private Integer aac004;
+    /**
+     * 雇佣日期 年月日
+     */
     private LocalDate aac005;
-    private int aac006;
+    /**
+     * 性别 -1 未知 0 女 1 男
+     */
+    private int aac006 = -1;
+    /**
+     * 身高
+     */
     private float aac007;
+    /**
+     * 体重
+     */
     private float aac008;
+    /**
+     * 兴趣爱好
+     */
     private List<String> aac009;
+    /**
+     * ab01信息
+     */
     private Ab01 ab01;
+    /**
+     * LocalDateTime时间 年月日 是分秒
+     */
     private LocalDateTime aac100;
+    /**
+     * Date时间 Date
+     */
     private Date aac101;
 }
