7b17a22df7d5529eeeacac0cb09414d6d2c3b4f6
LewJun <lewjun072@sina.com>
2020-08-10 11:05:06 +0800

    test1 -> testSomething

--- spring-boot-helloworld/src/test/java/com/example/lewjun/JunitTest.java
+++ spring-boot-helloworld/src/test/java/com/example/lewjun/JunitTest.java
@@ -2,23 +2,8 @@ package com.example.lewjun;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class JunitTest {
     @Test
-    public void test1() {
-        final List<Integer> integers = new ArrayList<>();
-        integers.add(1);
-        integers.add(2);
-        integers.add(3);
-        integers.add(4);
-        integers.add(4);
-        integers.add(1);
-
-        integers.remove(Collections.max(integers));
-        integers.remove(Collections.min(integers));
-        System.out.println(integers);
+    public void testSomething() {
     }
 }
