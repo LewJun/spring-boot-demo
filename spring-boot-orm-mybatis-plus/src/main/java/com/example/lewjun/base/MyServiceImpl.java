bbcfc566ed84eef7b22eca88bb9b8231f76c97b4
LewJun <lewjun072@sina.com>
2020-08-25 18:09:36 +0800

    spring 推荐在构造方法上注入属性

--- spring-boot-orm-mybatis-plus/src/main/java/com/example/lewjun/base/MyServiceImpl.java
+++ spring-boot-orm-mybatis-plus/src/main/java/com/example/lewjun/base/MyServiceImpl.java
@@ -4,10 +4,13 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MyServiceImpl<M extends MyBaseMapper<T>, T> extends ServiceImpl<M, T> implements MyIService<T> {
-    @Autowired
-    protected M myBaseMapper;
-
     public MyServiceImpl() {
+    }
+
+    // spring 推荐在构造方法上注入属性
+    @Autowired
+    public MyServiceImpl(final M myBaseMapper) {
+        this();
         super.baseMapper = myBaseMapper;
     }
 }
