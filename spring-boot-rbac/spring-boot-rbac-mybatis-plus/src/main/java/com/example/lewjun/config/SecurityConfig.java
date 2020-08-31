6100a0affafd1198f197ef88df761a68266c3144
LewJun <lewjun072@sina.com>
2020-08-31 09:47:38 +0800

    return PasswordEncoderFactories.createDelegatingPasswordEncoder();

--- spring-boot-rbac/spring-boot-rbac-mybatis-plus/src/main/java/com/example/lewjun/config/SecurityConfig.java
+++ spring-boot-rbac/spring-boot-rbac-mybatis-plus/src/main/java/com/example/lewjun/config/SecurityConfig.java
@@ -10,5 +10,6 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
