8698c7ea9485959f55d67eeb972e1f9d04e0d4f8
LewJun <lewjun072@sina.com>
2020-09-05 20:58:09 +0800

    重写toString方法

--- spring-boot-orm-mybatis-plus/src/main/java/com/example/lewjun/base/MyPageInfo.java
+++ spring-boot-orm-mybatis-plus/src/main/java/com/example/lewjun/base/MyPageInfo.java
@@ -7,10 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
 
 public class MyPageInfo<T> extends Page<T> {
-    protected List<T> records;
-    protected long total;
-    protected long size;
-    protected long current;
     @JsonIgnore
     protected long pages;
     @JsonIgnore
@@ -21,91 +17,23 @@ public class MyPageInfo<T> extends Page<T> {
     protected boolean searchCount;
     @JsonIgnore
     protected boolean hitCount;
-/*
 
-    @Override
-    public List<T> getRecords() {
-        return records;
+    public MyPageInfo(final long current, final long size) {
+        super(current, size);
     }
 
     @Override
-    public MyPageInfo<T> setRecords(final List<T> records) {
-        this.records = records;
-        return this;
+    public String toString() {
+        return "MyPageInfo{" +
+                "records=" + records +
+                ", total=" + total +
+                ", size=" + size +
+                ", current=" + current +
+                ", pages=" + pages +
+                ", orders=" + orders +
+                ", optimizeCountSql=" + optimizeCountSql +
+                ", searchCount=" + searchCount +
+                ", hitCount=" + hitCount +
+                '}';
     }
-
-    @Override
-    public long getTotal() {
-        return total;
-    }
-
-    @Override
-    public MyPageInfo<T> setTotal(final long total) {
-        this.total = total;
-        return this;
-    }
-
-    @Override
-    public long getSize() {
-        return size;
-    }
-
-    @Override
-    public MyPageInfo<T> setSize(final long size) {
-        this.size = size;
-        return this;
-    }
-
-    @Override
-    public long getCurrent() {
-        return current;
-    }
-
-    @Override
-    public MyPageInfo<T> setCurrent(final long current) {
-        this.current = current;
-        return this;
-    }
-
-    @Override
-    public List<OrderItem> getOrders() {
-        return orders;
-    }
-
-    @Override
-    public void setOrders(final List<OrderItem> orders) {
-        this.orders = orders;
-    }
-
-    @Override
-    public boolean isOptimizeCountSql() {
-        return optimizeCountSql;
-    }
-
-    @Override
-    public MyPageInfo<T> setOptimizeCountSql(final boolean optimizeCountSql) {
-        this.optimizeCountSql = optimizeCountSql;
-        return this;
-    }
-
-    @Override
-    public boolean isSearchCount() {
-        return searchCount;
-    }
-
-    @Override
-    public MyPageInfo<T> setSearchCount(final boolean searchCount) {
-        this.searchCount = searchCount;
-        return this;
-    }
-
-    @Override
-    public boolean isHitCount() {
-        return hitCount;
-    }
-
-    @Override
-    public void setHitCount(final boolean hitCount) {
-        this.hitCount = hitCount;
-    }*/
 }
