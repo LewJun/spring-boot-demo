4a5ef08904a1ce30a641bd0c8c8d32c67ffbe863
LewJun <lewjun072@sina.com>
2020-09-11 22:39:30 +0800

    解决empty encoded password问题

--- spring-boot-security/spring-boot-security-03-json-jwt/src/main/java/com/example/lewjun/model/LoginUser.java
+++ spring-boot-security/spring-boot-security-03-json-jwt/src/main/java/com/example/lewjun/model/LoginUser.java
@@ -6,7 +6,29 @@ import org.springframework.security.core.userdetails.User;
 import java.util.List;
 
 public class LoginUser extends User {
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> grantedAuthorities;
+
     public LoginUser(final String username, final String password, final List<GrantedAuthority> grantedAuthorities) {
         super(username, password, grantedAuthorities);
+
+        this.username = username;
+        this.password = password;
+        this.grantedAuthorities = grantedAuthorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public List<GrantedAuthority> getGrantedAuthorities() {
+        return grantedAuthorities;
     }
 }
