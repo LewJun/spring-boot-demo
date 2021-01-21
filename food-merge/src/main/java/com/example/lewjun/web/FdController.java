21dab678d40735a663b277f587a75bb3fa695999
LewJun <lewjun072@qq.com>
2021-01-21 14:19:47 +0800

    下载使用ResponseEntity<byte[]>，而不是HttpServletResponse

--- food-merge/src/main/java/com/example/lewjun/web/FdController.java
+++ food-merge/src/main/java/com/example/lewjun/web/FdController.java
@@ -5,15 +5,20 @@ import com.example.lewjun.domain.result.FdDownloadResult;
 import com.example.lewjun.domain.vo.FdQueryParam;
 import com.example.lewjun.domain.vo.FdUpdateParam;
 import com.example.lewjun.mapper.FdMapper;
+import com.example.lewjun.utils.FileDownloadUtil;
 import com.example.lewjun.utils.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @Controller
@@ -50,13 +55,20 @@ public class FdController {
         return JsonUtils.object2String(fdMapper.updateYyss001(fdUpdateParam));
     }
 
-    @GetMapping("/download")
-    public void download(final FdQueryParam param, final HttpServletResponse resp) throws IOException {
-        final List<FdDownloadResult> fdDownloadResults = fdMapper.findDownloadByParam(param);
+//    @GetMapping("/download")
+//    public void download(final FdQueryParam param, final HttpServletResponse resp) throws IOException {
+//        final List<FdDownloadResult> fdDownloadResults = fdMapper.findDownloadByParam(param);
+//
+//        resp.setContentType("application/vnd.ms-excel");
+//        resp.setCharacterEncoding("utf-8");
+//        resp.setHeader("Content-disposition", "attachment;filename=" + UUID.randomUUID() + ".xlsx");
+//        EasyExcel.write(resp.getOutputStream(), FdDownloadResult.class).sheet("Sheet 0").doWrite(fdDownloadResults);
+//    }
 
-        resp.setContentType("application/vnd.ms-excel");
-        resp.setCharacterEncoding("utf-8");
-        resp.setHeader("Content-disposition", "attachment;filename=" + UUID.randomUUID() + ".xlsx");
-        EasyExcel.write(resp.getOutputStream(), FdDownloadResult.class).sheet("Sheet 0").doWrite(fdDownloadResults);
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> download(final FdQueryParam param) throws IOException {
+        final File file = File.createTempFile(UUID.randomUUID().toString(), ".xls");
+        EasyExcel.write(file, FdDownloadResult.class).sheet("Sheet 0").doWrite(fdMapper.findDownloadByParam(param));
+        return FileDownloadUtil.getResponseEntity(file);
     }
 }
