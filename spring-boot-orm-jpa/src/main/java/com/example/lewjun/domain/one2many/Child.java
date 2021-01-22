f41150c9a78bafb6d307238fd8be467e424283e5
LewJun <lewjun072@qq.com>
2021-01-22 17:24:00 +0800

    没有必要在一对多的，多的一方添加@ManyToOne，JPA生成了一个中间表father_children，里面自由维护，所以mappedBy也没必要添加，optional也没必要

--- spring-boot-orm-jpa/src/main/java/com/example/lewjun/domain/one2many/Child.java
+++ spring-boot-orm-jpa/src/main/java/com/example/lewjun/domain/one2many/Child.java
@@ -17,4 +17,11 @@ public class Child {
     private Integer id;
 
     private String uname;
+
+//    没有必要再一对多的多的这一方使用@ManyToOne，JPA生成了一个中间表father_children，里面自有维护
+//    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH},
+//            // optional=false表示father不能为空，JPA生成了一个中间表father_children，里面自有维护
+//            optional = false
+//    )
+//    private Father father;
 }
--- spring-boot-orm-jpa/src/main/java/com/example/lewjun/domain/one2many/Father.java
+++ spring-boot-orm-jpa/src/main/java/com/example/lewjun/domain/one2many/Father.java
@@ -19,6 +19,8 @@ public class Father extends BaseObj {
     private String uname;
 
     @OneToMany(
+            // 没有必要再一对多的多的这一方使用@ManyToOne，JPA生成了一个中间表father_children，里面自有维护，所以mappedBy也就没必要了。
+            // mappedBy = "father",
             // 删除Father，会级联删除child
             cascade = CascadeType.ALL,
             // 懒加载
