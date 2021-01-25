249ebde67aef73a82409dd66e05f4701166db143
LewJun <lewjun072@qq.com>
2021-01-25 15:51:17 +0800

    @Data -> @Getter+@Setter

--- spring-boot-orm-jpa/src/main/java/com/example/lewjun/domain/Ab01.java
+++ spring-boot-orm-jpa/src/main/java/com/example/lewjun/domain/Ab01.java
@@ -1,22 +1,20 @@
 package com.example.lewjun.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
+@Getter
+@Setter
 @Accessors(chain = true)
 @Entity
 public class Ab01 extends BaseObj {
     /**
-     * 部门编号 如果不是id字段，那么需要使用@TableId注释这就是id
+     * 部门编号 如果不是id字段，那么需要使用@Id注释这就是id
      */
     @Id
     @GeneratedValue
