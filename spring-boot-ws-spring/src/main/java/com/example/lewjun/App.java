1dd3f875483e47b5157eae0609ee967a0b92597e
LewJun <lewjun072@sina.com>
2021-01-26 22:45:59 +0800

    init project

---
+++ spring-boot-ws-spring/src/main/java/com/example/lewjun/App.java
@@ -0,0 +1,23 @@
+package com.example.lewjun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * spring boot 启动类
+ */
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(final String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name", defaultValue = "World") final String name) {
+        return String.format("Hello %s!", name);
+    }
+}
